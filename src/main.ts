3731dbef17d9758299da14959ef0d53ab8e74a86
Jclark <justindanielclark@gmail.com>
2025-10-24 14:18:21 -0700

    + steve adj

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Controls } from "./models/Controls/Controls";
 import GifRenderTarget from "./models/RenderTargets/GifRenderTarget/GifRenderTarget";
 import HlsVideoRenderTarget from "./models/RenderTargets/HlsVideoTarget/HlsVideoRenderTarget";
 import SceneManager from "./models/SceneManager";
+import type { TVector3Limits } from "./types/TVector3";
 
 const markerDimensions = { x: 1, y: 0.57 };
 
@@ -15,6 +16,21 @@ const demoVideos = {
     "https://reel-em-in-hls-bucket.s3.us-west-1.amazonaws.com/4534bfc8-a5c5-4f1a-9c9d-a6a9d1875b43/playlist.m3u8",
   streetFighterWithAudio:
     "https://reel-em-in-hls-bucket.s3.us-west-1.amazonaws.com/bab4eed3-a93e-4dff-ae47-6a29195f2a23/playlist.m3u8",
+};
+
+const vectorRotationLimits: TVector3Limits = {
+  x: {
+    min: -30,
+    max: 30,
+  },
+  y: {
+    min: -30,
+    max: 30,
+  },
+  z: {
+    min: -30,
+    max: 30,
+  },
 };
 
 const hlsVideoTarget = new HlsVideoRenderTarget({
@@ -30,6 +46,7 @@ const hlsVideoTarget = new HlsVideoRenderTarget({
     x: 1,
     y: 0.57,
   },
+  vectorRotationLimits,
 });
 
 const gifTarget = new GifRenderTarget({
@@ -37,8 +54,8 @@ const gifTarget = new GifRenderTarget({
   gifUrl: "assets/testgif.gif",
   name: "Test Gif",
   dimensions: {
-    x: 0.35,
-    y: 0.204166667,
+    x: 0.7,
+    y: 0.408333334,
   },
   positionalOffsetVector: {
     x: 0,
@@ -56,6 +73,7 @@ const gifTarget = new GifRenderTarget({
     red: 5,
     tolerance: 10 / 255,
   },
+  vectorRotationLimits,
 });
 
 const gifTarget2 = new GifRenderTarget({
@@ -63,12 +81,12 @@ const gifTarget2 = new GifRenderTarget({
   gifUrl: "assets/cmslogo.gif",
   name: "CmsLogoGif",
   dimensions: {
-    x: 0.45,
-    y: 0.370588235,
+    x: 0.36,
+    y: 0.296470588,
   },
   positionalOffsetVector: {
     x: 0,
-    y: 1,
+    y: 1.2,
     z: 0,
   },
   originOffsetVector: {
@@ -76,32 +94,8 @@ const gifTarget2 = new GifRenderTarget({
     y: 1,
     z: 0,
   },
+  vectorRotationLimits,
 });
-// const gifTarget3 = new GifRenderTarget({
-//   markerDimensions,
-//   gifUrl: "assets/dancingbaby.gif",
-//   name: "DancingBabyGif",
-//   dimensions: {
-//     x: 0.75,
-//     y: 0.681818182,
-//   },
-//   positionalOffsetVector: {
-//     x: -1,
-//     y: 0,
-//     z: 0,
-//   },
-//   originOffsetVector: {
-//     x: -1,
-//     y: 0,
-//     z: 0,
-//   },
-//   transparencyTarget: {
-//     blue: 5,
-//     green: 5,
-//     red: 5,
-//     tolerance: 10 / 255,
-//   },
-// });
 
 new Controls({
   controls: [

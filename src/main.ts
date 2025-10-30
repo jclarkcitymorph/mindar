2e819f41a2679b8279af3102887806e673c9c669
JClark <justindanielclark@gmail.com>
2025-10-29 22:49:09 -0700

    Change to main to have full render

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import apiRetrieveMindFileObjUrl from "./api/apiRetrieveMindFileObjUrl";
 import apiRetrieveSiteConfig from "./api/apiRetrieveSiteConfig";
-// import type { Control } from "./models/Controls/Control/Control";
-// import MuteControl from "./models/Controls/Control/MuteControl/MuteControl";
-// import PlayPauseControl from "./models/Controls/Control/PlayPauseControl/PlayPauseControl";
-// import WeblinkControl from "./models/Controls/Control/WeblinkControl/WeblinkControl";
-// import { Controls } from "./models/Controls/Controls";
-// import HlsVideoRenderTarget from "./models/RenderTargets/HlsVideoTarget/HlsVideoRenderTarget";
-// import createRenderTargetsFromSiteConfiguration from "./utils/application/createRenderTargetsFromSiteConfiguration";
+import type { Control } from "./models/Controls/Control/Control";
+import MuteControl from "./models/Controls/Control/MuteControl/MuteControl";
+import PlayPauseControl from "./models/Controls/Control/PlayPauseControl/PlayPauseControl";
+import WeblinkControl from "./models/Controls/Control/WeblinkControl/WeblinkControl";
+import { Controls } from "./models/Controls/Controls";
+import HlsVideoRenderTarget from "./models/RenderTargets/HlsVideoTarget/HlsVideoRenderTarget";
+import createRenderTargetsFromSiteConfiguration from "./utils/application/createRenderTargetsFromSiteConfiguration";
 import SceneManager from "./models/SceneManager";
 import decomposeUrl from "./utils/web/decomposeUrl";
 
@@ -28,71 +28,71 @@ async function main() {
 
     const mindFileUrl = mindFileResp.data;
 
-    // const renderTargets = createRenderTargetsFromSiteConfiguration(
-    //   siteConfiguration.data
-    // );
-    // const videoTarget = renderTargets.find(
-    //   (t) => t instanceof HlsVideoRenderTarget
-    // );
+    const renderTargets = createRenderTargetsFromSiteConfiguration(
+      siteConfiguration.data
+    );
+    const videoTarget = renderTargets.find(
+      (t) => t instanceof HlsVideoRenderTarget
+    );
 
-    // const controls: Control[] = [];
+    const controls: Control[] = [];
 
-    // if (videoTarget) {
-    //   controls.push(
-    //     new PlayPauseControl({
-    //       state: "Paused",
-    //       onPointerDown: (state) => {
-    //         const video = videoTarget.getVideoObj();
-    //         switch (state) {
-    //           case "Playing": {
-    //             video?.pause();
-    //             break;
-    //           }
-    //           case "Pressed Playing": {
-    //             video?.pause();
-    //             break;
-    //           }
-    //           case "Paused": {
-    //             video?.play();
-    //             break;
-    //           }
-    //           case "Pressed Pause": {
-    //             video?.play();
-    //             break;
-    //           }
-    //         }
-    //       },
-    //     }),
-    //     new MuteControl({
-    //       state: "Sound Off",
-    //       onPointerDown: () => {
-    //         const video = videoTarget.getVideoObj();
-    //         if (video) {
-    //           if (video.muted) {
-    //             video.muted = false;
-    //             video.volume = 1;
-    //           } else {
-    //             video.muted = true;
-    //             video.volume = 0;
-    //           }
-    //         }
-    //       },
-    //     })
-    //   );
-    // }
+    if (videoTarget) {
+      controls.push(
+        new PlayPauseControl({
+          state: "Paused",
+          onPointerDown: (state) => {
+            const video = videoTarget.getVideoObj();
+            switch (state) {
+              case "Playing": {
+                video?.pause();
+                break;
+              }
+              case "Pressed Playing": {
+                video?.pause();
+                break;
+              }
+              case "Paused": {
+                video?.play();
+                break;
+              }
+              case "Pressed Pause": {
+                video?.play();
+                break;
+              }
+            }
+          },
+        }),
+        new MuteControl({
+          state: "Sound Off",
+          onPointerDown: () => {
+            const video = videoTarget.getVideoObj();
+            if (video) {
+              if (video.muted) {
+                video.muted = false;
+                video.volume = 1;
+              } else {
+                video.muted = true;
+                video.volume = 0;
+              }
+            }
+          },
+        })
+      );
+    }
 
-    // if (siteConfiguration.data.render.webLinkUrl) {
-    //   controls.push(
-    //     new WeblinkControl({
-    //       state: "Default",
-    //       weblinkUrl: siteConfiguration.data.render.webLinkUrl,
-    //     })
-    //   );
-    // }
+    if (siteConfiguration.data.render.webLinkUrl) {
+      controls.push(
+        new WeblinkControl({
+          state: "Default",
+          weblinkUrl: siteConfiguration.data.render.webLinkUrl,
+        })
+      );
+    }
 
-    // new Controls({
-    //   controls,
-    // });
+    new Controls({
+      controls,
+    });
 
     new SceneManager({
       isDebugging: false,
